6379a2b7772daf695e3a8d9cf0c93e92210bbb76
Harry Christiaens <harrychristiaens@MacBook-Pro-de-Harry.local>
2021-04-23 18:05:22 +0200

    Import old files

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class ArticleController extends AbstractController
+{
+
+    /**
+     * @Route("/url", name="ROUTE_NAME")
+     */
+    public function methodAction(Request $request)
+    {
+        // on renvoie une réponse qui contient de l'HTML à l'aide de twig.
+        return $this->render('article/ton_html.html.twig');
+    }
+ 
+     /**
+     * @Route("/about", name="about")
+     */
+    public function aboutAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('article/about.html.twig', []);
+    }
+    /**
+     * @Route("/ignaces", name="ignaces")
+     */
+    public function ignacesAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('article/ignaces.html.twig', []);
+    }
+
+    /**
+     * @Route("/antioche", name="antioche")
+     */
+    public function antiocheAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('article/antioche.html.twig', []);
+    }
+
+    /**
+     * @Route("/tertulien", name="tertulien")
+     */
+    public function tertulienAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('article/tertulien.html.twig', []);
+    }
+
+    /**
+     * @Route("/tsth", name="tsth")
+     */
+    public function tsthAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('article/tsth.html.twig', []);
+    }
+
+        /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('article/contact.html.twig', []);
+    }
+
+
+
+}
---
+++ src/Controller/MainController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        $articles = [
+            [
+                'title' => 'La ville d’Antioche, la ville de Homs en Syrie (Emeès)',
+                'route' => 'antioche',
+                'author' => 'Harry Christiaens',
+                'created' => \DateTime::createFromFormat('d-m-Y', '07-02-2017'),
+                'last_modified' => \DateTime::createFromFormat('d-m-Y', '07-02-2017'),
+                'tag' => [''],
+            ],
+            [
+                'title' => 'Ignaces',
+                'route' => 'ignaces',
+                'author' => 'Benjamin Ellis',
+                'created' => \DateTime::createFromFormat('d-m-Y', '04-11-2017'),
+                'last_modified' => \DateTime::createFromFormat('d-m-Y', '07-02-2017'),
+                'tag' => [''],
+            ],
+            [
+                'title' => 'Tertulien',
+                'route' => 'tertulien',
+                'author' => 'Harry',
+                'created' => \DateTime::createFromFormat('d-m-Y', '04-11-2017'),
+                'last_modified' => \DateTime::createFromFormat('d-m-Y', '07-02-2017'),
+                'tag' => [''],
+            ],
+        ];
+
+        // replace this example code with whatever you need
+        return $this->render('content/index.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('content/contact.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+}
